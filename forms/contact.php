a9e54581a0710db9d7bd5b7f13456d0a697f36c1
remo-develope <developersremo@gmail.com>
2021-12-14 08:10:52 +0700

    tambah file

---
+++ forms/contact.php
@@ -0,0 +1,21 @@
+<! DOCTYPE html>  
+    <html>  
+    <head>  
+    <title>Thanks</title>  
+    <meta name="viewport" content="width=device-width, initial-scale=1">  
+    <meta charset ="utf-8">  
+    <style>  
+    body {  
+        background-color: #fff;  
+    }  
+    </style>  
+
+       <script type ="text/JavaScript">  
+       		alert("Thanks for your message");
+       </script>
+   
+    </head>  
+    <body>
+    
+    </body>  
+    </html>  
